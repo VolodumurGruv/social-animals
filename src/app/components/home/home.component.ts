620d3b85b5f485966ebb6f54c22cabb26c5e55c3
VolodumurGruv <gru.v@i.ua>
2024-09-07 01:46:40 +0300

    about section is done

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AdvertismentComponent } from '../advertisment/advertisment.component';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AdvertismentComponent],
+  imports: [AdvertismentComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
